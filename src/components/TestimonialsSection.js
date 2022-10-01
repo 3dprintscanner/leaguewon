00d8369a148a201e76fdf131f1401c9a9bf17f3d
Anthony <anthony@opencell.bio>
2022-10-01 14:41:43 +0100

    first commit

---
+++ src/components/TestimonialsSection.js
@@ -0,0 +1,94 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from "@material-ui/core/styles";
+import Section from "./Section";
+import SectionHeader from "./SectionHeader";
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    paddingTop: 0,
+  },
+  avatar: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+}));
+
+function TestimonialsSection(props) {
+  const classes = useStyles();
+
+  const items = [
+    {
+      avatar: "https://uploads.divjoy.com/pravatar-150x-5.jpeg",
+      name: "Marc",
+      testimonial:
+        "Great potential, the visibility of other traders is great together with supporting the learning of users",
+      company: "Company",
+    },
+    {
+      avatar: "https://uploads.divjoy.com/pravatar-150x-48.jpeg",
+      name: "Michael",
+      testimonial:
+        "I like the way investors can stake or follow individual traders",
+      company: "Company",
+    },
+    {
+      avatar: "https://uploads.divjoy.com/pravatar-150x-12.jpeg",
+      name: "Thomas",
+      testimonial:
+        "Fixes the black box model of firms like eToro and Gate.io and makes that information publibly available",
+      company: "Company",
+    },
+  ];
+
+  return (
+    <Section
+      bgColor={props.bgColor}
+      size={props.size}
+      bgImage={props.bgImage}
+      bgImageOpacity={props.bgImageOpacity}
+    >
+      <Container>
+        <SectionHeader
+          title={props.title}
+          subtitle={props.subtitle}
+          size={4}
+          textAlign="center"
+        />
+        <Grid container={true} justifyContent="center" spacing={4}>
+          {items.map((item, index) => (
+            <Grid item={true} xs={12} sm={4} key={index}>
+              <Card>
+                <CardContent>
+                  <Typography variant="body1" component="p">
+                    "{item.testimonial}"
+                  </Typography>
+                </CardContent>
+                <CardHeader
+                  className={classes.header}
+                  avatar={
+                    <Avatar
+                      src={item.avatar}
+                      alt={item.name}
+                      className={classes.avatar}
+                    />
+                  }
+                  title={item.name}
+                  subheader={item.company}
+                />
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Section>
+  );
+}
+
+export default TestimonialsSection;
---
+++ src/pages/index.js
@@ -0,0 +1,76 @@
+import React from "react";
+import Meta from "./../components/Meta";
+import HeroSection from "./../components/HeroSection";
+import FeaturesSection from "./../components/FeaturesSection";
+import ClientsSection from "./../components/ClientsSection";
+import TestimonialsSection from "./../components/TestimonialsSection";
+import NewsletterSection from "./../components/NewsletterSection";
+import CtaSection from "./../components/CtaSection";
+
+function IndexPage(props) {
+  return (
+    <>
+      <Meta />
+      <HeroSection
+        bgColor="primary"
+        size="large"
+        bgImage="https://source.unsplash.com/ugnrXk1129g/1600x800"
+        bgImageOpacity={0.3}
+        title="RankDEX"
+        subtitle="Find, follow, learn and earn from the best crypto traders on the DEXes"
+        buttonText="Join the Community"
+        buttonColor="default"
+        buttonPath="/pricing"
+      />
+      <FeaturesSection
+        bgColor="default"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="Features"
+        subtitle=""
+      />
+      <ClientsSection
+        bgColor="light"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="You're in good company"
+        subtitle=""
+      />
+      <TestimonialsSection
+        bgColor="default"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="Here's what people are saying"
+        subtitle=""
+      />
+      <NewsletterSection
+        bgColor="light"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="Stay in the know"
+        subtitle="Receive our latest articles and feature updates"
+        buttonText="Subscribe"
+        buttonColor="primary"
+        inputPlaceholder="Enter your email"
+        subscribedMessage="You are now subscribed!"
+      />
+      <CtaSection
+        bgColor="primary"
+        size="medium"
+        bgImage=""
+        bgImageOpacity={1}
+        title="Ready to get started?"
+        subtitle=""
+        buttonText="Get Started"
+        buttonColor="default"
+        buttonPath="/pricing"
+      />
+    </>
+  );
+}
+
+export default IndexPage;
